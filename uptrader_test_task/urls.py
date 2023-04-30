8377b1183f509b5e1453f7602201a3523e1f5b87
vital <375257932770>
2023-04-30 13:24:01 +0300

    nester menu

---
+++ uptrader_test_task/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, re_path, include
+from menu.views import index
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('__debug__/', include('debug_toolbar.urls')),
+    re_path(r'^(.*)/$', index, name='index')
+]
